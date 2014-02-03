b0b807769c149c9eeec7589ef675dcda69457273
Alexey Naydenov <alexey.naydenov@linux.com>
2014-02-03 11:57:17 +0400

    Add scafolding for testing reader on samples.

---
+++ vartools/test/test_samples.py
@@ -0,0 +1,20 @@
+"""Compare TraceReader output for samples with expected values."""
+
+import os
+import vartools.tracereader as vttr
+
+DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+def check_empty(trace):
+    print('ok')
+
+
+TEST_FUNCTION_FILE = [(check_empty, 'empty.bin')]
+
+
+def test_samples():
+    for function, filename in TEST_FUNCTION_FILE:
+        with open(os.path.join(DATA_PATH, filename), 'rb') as sample:
+            trace = vttr.TraceReader(sample)
+            yield function, trace
